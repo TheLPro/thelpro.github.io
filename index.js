0b4b7d728aa86bf1d7e317c108de27b957cbfccc
Leon <leon.potocki@gmail.com>
2023-11-04 13:28:14 +0000

    a

---
+++ index.js
@@ -0,0 +1,40 @@
+//light / dark mode changer
+
+const varToString = varObj => Object.keys(varObj)[0];
+const button = document.querySelector("#theme");
+window.localStorage.setItem("theme", "light");
+
+var light = new Map();
+light.set("--text", "#040301");
+light.set("--bg", "#FFFDFB");
+light.set("--primary", "#F49434");
+light.set("--secondary", "#8DCFCF");
+
+var dark = new Map();
+
+dark.set("--text", "#FEFDFB");
+dark.set("--bg", "#0F0E0B");
+dark.set("--primary", "#CB6B0B");
+dark.set("--secondary", "#347A7A");
+
+function buttonClick() {
+    console.log("hi")
+    if (window.localStorage.getItem("theme") == "light") {
+        for (let [key, value] of dark) {
+            document.documentElement.style.setProperty(key, value);
+        }
+        updateTheme("dark");
+        button.innerHTML = "Dark";
+    } else if (window.localStorage.getItem("theme") == "dark") {
+        for (let [key, value] of light) {
+            document.documentElement.style.setProperty(key, value);
+        }
+        updateTheme("light");
+        button.innerHTML = "Light";
+    }
+}
+
+function updateTheme(theme) {
+    window.localStorage.setItem("theme", theme);
+}
+button.addEventListener("click", buttonClick);
